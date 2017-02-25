47535247a82b9bc1f748f818fd52b136ed8e92dc
Katarzyna <katarzyna.konczalska@gmail.com>
2017-02-25 21:27:01 +0100

    comm

---
+++ GigerEx/Models/Band.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GigerEx.Models
+{
+    public class Band
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+    }
+
+    public class BandDBContext : DbContext
+    {
+        public DbSet<Band> Bands { get; set; }
+    }
+}
